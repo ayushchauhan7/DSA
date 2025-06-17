f9a01ae1f81dd6e940ef66b1b09c045352b61b1c
ayushchauhan7 <chauhan0007ayush@gmail.com>
2025-06-17 20:05:15 +0530

    second

--- src/Grading_System.java
+++ src/Grading_System.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Grading_System {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        if (a > 90) {
+            System.out.println("Excellent");
+        }
+        else if (a > 80 && a <= 90) {
+            System.out.println("Good");
+        }
+        else if (a > 70 && a <= 80) {
+            System.out.println("Fair");
+        }
+        else if (a > 60 && a <= 70) {
+            System.out.println("Meets Expectations");
+        }
+        else {
+            System.out.println("Below Par");
+        }
+    }
 }
--- src/Prime_Number.java
+++ src/Prime_Number.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Prime_Number {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = s.nextInt();
+        }
+    }
 }
